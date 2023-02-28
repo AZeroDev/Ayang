84a1ed4aa9666f8885cb8f7276f87627fdfbccae
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-02-28 18:34:16 +0700

    update

--- src/commands/pengembang/eval.js
+++ src/commands/pengembang/eval.js
@@ -1,8 +1,48 @@
-import { SlashCommandBuilder } from "discord.js";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+
+import { inspect } from "node:util";
 
 export default {
-    data: new SlashCommandBuilder(),
-    execute: (interaction) => {
-        
+    data: new SlashCommandBuilder()
+        .setName("eval")
+        .setDescription("Evaluasi kode pemrograman")
+        .addStringOption(option => 
+            option.setName("kode-program")
+                .setDescription("Sebuah kode pemrograman basis JavaScript")
+        ),
+    execute: async(interaction) => {
+        const { colors } = interaction.client.config;
+        const code = interaction.options.getString("kode-program");
+        const embed = new EmbedBuilder().setColor(colors.default);
+
+        await interaction.deferReply();
+
+        try {
+            let evaled = await eval(code);
+                evaled = clean(evaled);
+
+            embed.setTitle("Respon Hasil")
+            embed.setDescription(`\`\`\`js\n${evaled}\`\`\``);
+
+            await interaction.editReply({ embeds: [embed] });
+        }
+        catch(error) {
+            embed.setColor(colors.error);
+            embed.setTitle("Respon Error!")
+            embed.setDescription(`\`\`\`js\n${clean(error)}\`\`\``);
+
+            await interaction.editReply({ embeds: [embed] })
+        }
     }
 }
+
+function clean(code) {
+    if  (typeof code === "string") {
+        return code
+            .replace(/`/g, "`" + String.fromCharCode(8203))
+            .replace(/@/g, "@" + String.fromCharCode(8203));
+    }
+    else {
+        return inspect(code, { depth: 0 });
+    }
+};
