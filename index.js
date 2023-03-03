68d8cd5191dc43b210b8f934293849893890b7cf
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-03-03 19:09:51 +0700

    up

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 process.on("unhandledRejection", error => console.error(error));
 process.on("uncaughtException", error => console.error(error));
-process.on("warning", (warn) => console.warn("[Warning!]", warn.name, warn.message));
 
 import("./src/index.js");
