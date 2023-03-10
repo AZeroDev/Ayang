7c1041f2c8728bb9f1c5be6b28023e8b38d9bc91
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-03-10 14:27:30 +0700

    try

--- src/structures/RankCanvas.js
+++ src/structures/RankCanvas.js
@@ -101,10 +101,10 @@ export class RankCanvas {
 
                 // Xp
                 .setTextAlign("right")
-                .printText("XP:", width-239-canvas.measureText(this.data.xp.current+" / "+this.data.xp.target) - 5, 210)
-                .printText(this.data.xp.current.toString(), width-239-canvas.measureText("/ "+this.data.xp.target).width - 5, 210)
-                .printText("/", width-canvas.measureText(this.data.xp.target).width - 5, 210)
-                .printText(this.data.xp.target, width-canvas.measureText("XP: "+this.data.xp.current+" /").width - 5, 210)
+                .printText("XP:", (width-40)-canvas.measureText(this.data.xp.current+" / "+this.data.xp.target) - 5, 210)
+                .printText(this.data.xp.current.toString(), (width-40)-canvas.measureText("/ "+this.data.xp.target).width - 5, 210)
+                .printText("/", (width-40)-canvas.measureText(this.data.xp.target).width - 5, 210)
+                .printText(this.data.xp.target, (width-40) - 5, 210)
             )
 
             // progress bar
