1ddf7cd964371d29f3d3f7bc795bc4d26f37b0b9
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-03-11 03:15:09 +0700

    update

--- src/commands/leveling/top.js
+++ src/commands/leveling/top.js
@@ -38,7 +38,7 @@ async function sendTopGlobal(interaction) {
     const embed = new EmbedBuilder()
         .setColor(interaction.client.config.colors.default)
         .setTitle("Peringkat Global - Top 10 besar leveling")
-        .setDescription("```\nmd"+data.map(e => `# ${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString().replaceAl(",",".")}`).join("\n")+"```");
+        .setDescription("```\nmd"+data.map(e => `# ${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString().replaceAll(",",".")}`).join("\n")+"```");
 
     await interaction.editReply({ embeds: [embed] });
 }
